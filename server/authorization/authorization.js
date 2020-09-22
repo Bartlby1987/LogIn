d60e5207a1a0fb88b1515aaa8e3896b5f21b1cab
bartlby1987 <bartlby1987@gmail.com>
2020-09-22 13:25:48 +0300

    sqlite. authorization panel.

--- server/authorization/authorization.js
+++ server/authorization/authorization.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('db.sqlite3');
-const notNeedInfoKey = ["password", "profileInfo"];
+// const notNeedInfoKey = ["password", "profileInfo"];
 const TokenGenerator = require('uuid-token-generator');
 const tokenObj = new TokenGenerator(256, TokenGenerator.BASE62);
 
@@ -23,23 +23,8 @@ async function execAsync(sql, params) {
     })
 }
 
-
-// function generateToken(sessionTokens) {
-//     if (sessionTokens.length === 0) {
-//         return token
-//     }
-//     for (let i = 0; i < sessionTokens.length; i++) {
-//         if (sessionTokens[i] !== token) {
-//             return token
-//         }
-//     }
-//     generateToken()
-// }
-
-
 async function authorizeUser(loginPassword) {
     return new Promise(async (resolve, reject) => {
-
         try {
             let hashPassword =crypto.createHash('md5').update(loginPassword["password"]).digest('hex');
 
@@ -70,19 +55,14 @@ async function authorizeUser(loginPassword) {
                 let params = [token, sessionUserData["name"], sessionUserData["email"],
                     sessionUserData["login"], sessionUserData["password"], sessionUserData["profileInfo"]]
                 await execAsync("INSERT INTO usersSession VALUES (?,?,?,?,?,?)", params);
-
-
+                let sqUserData = `SELECT sessionId,name,login,email  FROM usersSession WHERE sessionId='${token}'`;
+                resolve(await execAsync(sqUserData))
             }
-
-
-            // let params = [registrationData["name"], registrationData["email"],
-            //     registrationData["login"], hashPassword(registrationData["password"]), registrationData["profileInfo"]];
-            // await execAsync("INSERT INTO users VALUES (?,?,?,?,?)", params);
         } catch (error) {
             reject("technical issue");
         }
     }
-)
+    )
 }
 
 // const deleteKeyFromObj = (object, keysArray) => {
@@ -92,40 +72,40 @@ async function authorizeUser(loginPassword) {
 //     return object
 // }
 
-function getSessionValue() {
-    return Object.keys(session)
-}
+// function getSessionValue() {
+//     return Object.keys(session)
+// }
+//
+// function logOutFromSession() {
+//     session = {};
+// }
 
-function logOutFromSession() {
-    session = {};
-}
-
-function authorizeUser(loginPassword, token) {
-    let personsInfo = fs.readFileSync("registration-database/database.json", "utf8");
-    let personsInfoObj;
-    let hashedPassword = crypto.createHash('md5').update(loginPassword["password"]).digest('hex');
-    let login = loginPassword["login"];
-    if (personsInfo === "") {
-        return badResponse
-    }
-    personsInfoObj = JSON.parse(personsInfo);
-    let mapPersonsInfoObj = new Map(Object.entries(personsInfoObj))
-    for (let [, value] of mapPersonsInfoObj) {
-        if (value.login === login && value.password === hashedPassword) {
-            session[token] = JSON.parse(JSON.stringify(value))
-            return deleteKeyFromObj(value, notNeedInfoKey)
-        }
-    }
-    return badResponse
-}
-
-function getProfileInfo(token) {
-    return session[token]
-}
+// function authorizeUser(loginPassword, token) {
+//     let personsInfo = fs.readFileSync("registration-database/database.json", "utf8");
+//     let personsInfoObj;
+//     let hashedPassword = crypto.createHash('md5').update(loginPassword["password"]).digest('hex');
+//     let login = loginPassword["login"];
+//     if (personsInfo === "") {
+//         return badResponse
+//     }
+//     personsInfoObj = JSON.parse(personsInfo);
+//     let mapPersonsInfoObj = new Map(Object.entries(personsInfoObj))
+//     for (let [, value] of mapPersonsInfoObj) {
+//         if (value.login === login && value.password === hashedPassword) {
+//             session[token] = JSON.parse(JSON.stringify(value))
+//             return deleteKeyFromObj(value, notNeedInfoKey)
+//         }
+//     }
+//     return badResponse
+// }
+//
+// function getProfileInfo(token) {
+//     return session[token]
+// }
 
 module.exports = {
-    authorizeUser: authorizeUser,
-    getProfileInfo: getProfileInfo,
-    getSessionValue: getSessionValue,
-    logOutFromSession: logOutFromSession
+    authorizeUser: authorizeUser
+    // getProfileInfo: getProfileInfo,
+    // getSessionValue: getSessionValue,
+    // logOutFromSession: logOutFromSession
 }
